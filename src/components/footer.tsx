84591a0abb95d8d2b0fe8a25dc942da98d6adbda
Brianca Knight <briancaknight@gmail.com>
2024-03-28 14:25:32 -0700

    refactor header and footer with logo

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/prismicio";
 import { PrismicNextLink } from "@prismicio/next";
 import Link from "next/link";
+import Logo from "@/components/Logo";
+import Bounded from "@/components/Bounded"
 
 
 export default async function Footer() {
@@ -9,20 +11,22 @@ export default async function Footer() {
 
   const settings = await client.getSingle("settings");
 
-  return <footer>
-    <Link href="/">{settings.data.site_title}</Link>
+return <Bounded as="footer" >
+    <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+      <Link href="/"><Logo /></Link>
 
-    <p>©{new Date().getFullYear()}</p>
+      <p className="text-xs">
+        ©{new Date().getFullYear()}</p>
 
-    <ul>
-      {settings.data.navigation.map(({ link, label }) => (
-        <li key={label}>
-          <PrismicNextLink field={link}>{label}</PrismicNextLink>
-        </li>
-      ))}
-    </ul>
+      <ul className="flex">
+        {settings.data.navigation.map(({ link, label }) => (
+          <li key={label}>
+            <PrismicNextLink field={link} className="p-3">{label}</PrismicNextLink>
+          </li>
+        ))}
+      </ul>
 
 
-
-  </footer>
+    </div>
+  </Bounded>
 }
